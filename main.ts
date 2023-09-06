878ae97ad3ba542e483e662f89b204b806ff85e3
Nour Domani <138979883+NourDomani@users.noreply.github.com>
2023-09-07 00:11:58 +0300

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,8 +1,14 @@
+makerbit.onIrButton(IrButton.NUM6, IrButtonAction.Pressed, function () {
+    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinLeft)
+})
 makerbit.onIrButton(IrButton.NUM3, IrButtonAction.Pressed, function () {
     mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
 })
 makerbit.onIrButton(IrButton.Light, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Pinkish)
+})
+makerbit.onIrButton(IrButton.NUM9, IrButtonAction.Pressed, function () {
+    mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_SpinRight)
 })
 makerbit.onIrButton(IrButton.Right, IrButtonAction.Pressed, function () {
     mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Right, 103)
@@ -23,7 +29,7 @@ makerbit.onIrButton(IrButton.Down, IrButtonAction.Pressed, function () {
     mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
 })
 makerbit.onIrButton(IrButton.NUM4, IrButtonAction.Pressed, function () {
-    mbit_Robot.RGB_Car_Program().showBarGraph(0, 255)
+    mbit_Robot.RGB_Car_Program().showBarGraph(9, 98)
 })
 makerbit.onIrButton(IrButton.NUM2, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Program().showRainbow(1, 360)
@@ -32,6 +38,9 @@ makerbit.onIrButton(IrButton.Up, IrButtonAction.Pressed, function () {
     mbit_Robot.CarCtrlSpeed(mbit_Robot.CarState.Car_Run, 103)
     basic.pause(2000)
     mbit_Robot.CarCtrl(mbit_Robot.CarState.Car_Stop)
+})
+makerbit.onIrButton(IrButton.NUM5, IrButtonAction.Pressed, function () {
+    basic.showIcon(IconNames.Happy)
 })
 makerbit.onIrButton(IrButton.Power, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.OFF)
@@ -43,7 +52,7 @@ makerbit.onIrButton(IrButton.NUM0, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Cyan)
 })
 makerbit.onIrButton(IrButton.BEEP, IrButtonAction.Pressed, function () {
-    mbit_Music.Buzzer(DigitalPin.P2, mbit_Music.enBuzzer.Beep)
+    mbit_Robot.Music_Car(mbit_Robot.enMusic.power_up)
 })
 makerbit.onIrButton(IrButton.Plus, IrButtonAction.Pressed, function () {
     mbit_Robot.RGB_Car_Big2(mbit_Robot.enColor.Green)
